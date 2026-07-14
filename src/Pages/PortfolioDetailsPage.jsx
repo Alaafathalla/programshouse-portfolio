1a13aca992db364e44f1cdd4054c5281452d5560
Alaafathalla <alaafathalla36@gmail.com>
2026-07-14 11:45:09 +0300

    about proj sec

--- src/Pages/PortfolioDetailsPage.jsx
+++ src/Pages/PortfolioDetailsPage.jsx
@@ -1,11 +1,13 @@
 import { Goal } from "lucide-react";
 import Hero from "../components/PortfolioDetailsComponents/Hero/Hero";
 import Goals from "../components/PortfolioDetailsComponents/Goals";
+import AboutProjectSection from "../components/PortfolioDetailsComponents/AboutProjectSection ";
 export default function PortfolioDetailsPage() {
   return (
     <main className="min-h-screen bg-[#111521]">
       <Hero />
       <Goals />
+      <AboutProjectSection />
     </main>
   );
 }
